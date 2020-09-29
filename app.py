226a35a1589954b7ea725a1174c4e086fdebc1ab
annuc1s <annuciis121@gmail.com>
2020-09-29 20:11:55 +0000

    Connecting Flask to MongoDB and looping through the collection

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+#database name and uri linking to that database
+app.config["MONGO_DBNAME"] = 'munch_menu'
+app.config["MONGO_URI"] = 'mongodb+srv://RGz8TLF1RmPmQvu9:MunCH_ms3@cluster0.gsp9j.mongodb.net/munch_menu?retryWrites=true&w=majority'
+
+#app added using the constructor method
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return render_template("index.html")
+@app.route('/get_items')
+
+def get_items():
+#call items from MONGO collection menu_items
+    return render_template("index.html", items=mongo.db.menu_items.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
