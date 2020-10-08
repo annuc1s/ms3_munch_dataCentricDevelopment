79f461b0c01b1e69cce3cb2924753b00240ba19e
annuc1s <annuciis121@gmail.com>
2020-10-08 16:38:41 +0000

    routing for sign_up created with encrypted password

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_menu')
-
 def get_menu():
 #call items from MONGO collection menu_items
     return render_template("index.html", 
@@ -29,11 +28,36 @@ def get_menu():
 
 @app.route('/sign_in', methods= ['POST', 'GET'])
 def sign_in():
-    if request.method == "POST":
-        user_login = users.find_one({'name'})
-        )
+    return render_template ("sign-in.html")
+
+
+# Routing through registering a new user and encrypting their password for security
+@app.route('/sign_up', methods= ['POST', 'GET'])
+def sign_up():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            # if there isn't an existing user with the chosen username
+            # a new user is added to the database
+            # and the password is encrypted (hashed)
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass })
+            session['username'] = request.form['username']
+            # After creating new user date they are redirected to 'book_tbl'
+            return redirect (url_for('book_table'))
+
+        return  'This username already exists!'
+    return render_template('sign-in.html')
+
+
+@app.route('/book_table')
+def book_table():
+    return render_template("book-tbl.html")
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
