c51d13834639b1a901a98fce6295e9c339c94a6d
annuc1s <annuciis121@gmail.com>
2020-10-10 18:10:23 +0000

    Python code tidied up following reccommendations of PEP8.org

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import os
-from flask import (
-    Flask, 
-    render_template, 
-    request, redirect, 
-    url_for, session, 
-)
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import bcrypt
@@ -26,6 +21,7 @@ burgers = mongo.db.gourmet_burgers.find()
 toppings = mongo.db.toppings_sides.find()
 milkshakes = mongo.db.milkshakes.find()
 
+
 @app.route('/')
 @app.route('/home_page')
 def home_page():
@@ -38,9 +34,12 @@ def home_page():
                            )
 
 #Creates a route to sign-in.html
+
+
 @app.route('/log_in')
 def log_in():
     return render_template("sign-in.html")
+
 
 # Routing through existing users and decrypting their password from database
 @app.route('/sign_in', methods=['POST'])
@@ -58,6 +57,7 @@ def sign_in():
     #If the details are not valid it will reload sign-in.html
     return redirect(url_for('log_in'))
 
+
 # Routing through registering a new user and encrypting their password for security
 @app.route('/sign_up', methods=['POST', 'GET'])
 def sign_up():
@@ -66,9 +66,9 @@ def sign_up():
         existing_user = users.find_one({'name': request.form['username']})
 
         if existing_user is None:
-            # if there isn't an existing user with the chosen username
-            # a new user is added to the database
-            # and the password is encrypted (hashed)
+            """ if there isn't an existing user with the chosen username
+             a new user is added to the database
+             and the password is encrypted (hashed)"""
             hashpass = bcrypt.hashpw(
                 request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert(
@@ -86,6 +86,7 @@ def sign_up():
 def book_table():
     return render_template("book-tbl.html")
 
+
 #Allows reservation form to be submitted
 #and stores data in MongoDB within a reservations collection
 #after successful submission, the user is redirected to edit_reservation
@@ -102,12 +103,13 @@ def reserve_table():
         "reservation_time": request.form.get("reservation_time"),
         "party_size": request.form.get("party_size"),
         "additional_info": request.form.get("additional_info")
-    }
+        }
     id=reservations.insert_one(make_reservation)
     #reservations.insert_one(request.form.to_dict())
     return redirect(url_for('view_reservation', reservations_id=id.inserted_id))
 
-#retrieving data from users database to display in review-reservations.html & edit-reservations.html
+
+#retrieving data from users database to display in review-reservations.html
 @app.route('/view_reservation/<reservations_id>', methods=['GET'])
 def view_reservation(reservations_id):
     reserved = mongo.db.reservations.find_one({'_id': ObjectId(reservations_id)})
